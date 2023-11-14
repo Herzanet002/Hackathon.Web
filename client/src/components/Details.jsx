0a8656fc3d2216b11beb01fdc856c4166b3faea9
Степан Стоянов <stoyanovstepan@yandex.ru>
2023-11-14 12:16:10 +0500

    add backend endpoint

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import {  
     Box,
     Button,
@@ -19,154 +20,168 @@ const Details = () => {
             patronymic: data.get('patronymic'),
             age: data.get('age'),
             email: data.get('email'),
-            expirience: data.get('expirience'),
+            experience: data.get('experience'),
             profession: data.get('profession')
         });
+        axios.post('http://localhost:5043/api/participants/register', 
+        {
+        firstName: data.get('firstName'),
+        lastName: data.get('firstName'),
+        patronymic: data.get('patronymic'),
+        age: data.get('age'),
+        email: data.get('email'),
+        experience: data.get('experience'),
+        profession: data.get('profession')
+    }).then(function (response) {                  
+        // Handle the response data
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        // Handle any errors
+        console.error(error);
+      });
     }
 
 
     return (
-        <Stack 
-        component='section'
-        direction="row"
-        justifyContent= 'left'
-        alignItems='center'
-        sx={{
+        <Stack
+          component='section'
+          direction="row"
+          justifyContent='left'
+          alignItems='center'
+          sx={{
             py: 10,
             px: 2,
-            
-        }}
-        style={{ 
-            height: "72vh",
-        }}
+            height: '72vh',
+          }}
         >
-            <Stack
-            component='section'
-            direction="column"
-            justifyContent= 'left'
-            >
-            <Title 
-            text={
-                'Стало интерестно?'
-                } 
-            textAlign={'center'}
-            />
-            <Paragraph 
-            text={
+          {/* Left Stack */}
+          <Stack component='section' direction="column" justifyContent='left'>
+            <Title text={'Стало интересно?'} textAlign={'center'} />
+            <Paragraph
+              text={
                 'Для получения дополнительной информации о регистрации, программе и других вопросах, пожалуйста, свяжитесь с нами.'
-            }
-            maxWidth = {'sm'}
-            mx={0}
-            textAlign={'center'}
+              }
+              maxWidth={'sm'}
+              mx={0}
+              textAlign={'center'}
             />
-            </Stack>
-            
-            <Box
-component="form"
-noValidate
-onSubmit={handleSubmit}
-sx={{
-width: '40%', // Устанавливаем ширину на половину экрана
-mx: 'auto', // Центрируем по горизонтали
-mt: 1,
-py: 2,
-}}
->
-<TextField
-margin="normal"
-size='small'
-required
-fullWidth
-id="firstName"
-label="Имя"
-name="firstName"
-autoFocus
-className='validate'
-/>
-<TextField
-margin="normal"
-size='small'
-required
-id="lastName"
-label="Фамилия"
-name="lastName"
-fullWidth
-className='validate'
-/>
-<TextField
-margin="normal"
-size='small'
-id="patronymic"
-label="Отчество"
-name="patronymic"
-fullWidth
-className='validate'
-/>
-<TextField
-margin="normal"
-size='small'
-required
-fullWidth
-id="age"
-label="Возраст"
-name="age"
-className='validate'
-/>
-<TextField
-margin="normal"
-size='small'
-required
-fullWidth
-id="email"
-label="Email адрес"
-name="email"
-autoComplete="email"
-className='validate'
-/>
-<TextField
-size='small'
-margin="normal"
-required
-fullWidth
-id="experience"
-label="Опыт работы"
-name="experience"
-className='validate'
-/>
-<TextField
-size='small'
-margin="normal"
-required
-fullWidth
-id="profession"
-label="Профессия"
-name="profession"
-className='validate'
-/>
-<Button
-variant="contained"
-type="submit"
-size="medium"
-sx={{
-fontSize: '0.9rem',
-textTransform: 'capitalize',
-py: 1,
-mt: 3,
-mb: 2,
-borderRadius: 1, // Здесь устанавливаем значение закругления (можете изменить на свое усмотрение)
-backgroundColor: '#14192d',
-"&:hover": {
-backgroundColor: '#1e2a5a',
-}
-}}
->
-Отправить
-</Button>
-</Box>
-
-
+          </Stack>
+    
+          {/* Right Box */}
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{
+              width: '40%',
+              mx: 'auto', // Center the form horizontally
+              mt: 1,
+              py: 2,
+            }}
+          >
+            {/* TextField components */}
+            <TextField
+              margin="normal"
+              size="small"
+              required
+              fullWidth
+              id="firstName"
+              label="Имя"
+              name="firstName"
+              autoFocus
+              className='validate'
+            />
+    
+            <TextField
+              margin="normal"
+              size="small"
+              required
+              fullWidth
+              id="lastName"
+              label="Фамилия"
+              name="lastName"
+              className='validate'
+            />
+    
+            <TextField
+              margin="normal"
+              size="small"
+              id="patronymic"
+              label="Отчество"
+              name="patronymic"
+              fullWidth
+              className='validate'
+            />
+    
+            <TextField
+              margin="normal"
+              size="small"
+              required
+              fullWidth
+              id="age"
+              label="Возраст"
+              name="age"
+              className='validate'
+            />
+    
+            <TextField
+              margin="normal"
+              size="small"
+              required
+              fullWidth
+              id="email"
+              label="Email адрес"
+              name="email"
+              autoComplete="email"
+              className='validate'
+            />
+    
+            <TextField
+              size='small'
+              margin="normal"
+              required
+              fullWidth
+              id="experience"
+              label="Опыт работы"
+              name="experience"
+              className='validate'
+            />
+    
+            <TextField
+              size='small'
+              margin="normal"
+              required
+              fullWidth
+              id="profession"
+              label="Профессия"
+              name="profession"
+              className='validate'
+            />
+    
+            {/* Submit Button */}
+            <Button
+              variant="contained"
+              type="submit"
+              size="medium"
+              onSubmit={handleSubmit}
+              sx={{
+                fontSize: '0.9rem',
+                textTransform: 'capitalize',
+                py: 1,
+                mt: 3,
+                mb: 2,
+                borderRadius: 1,
+                backgroundColor: '#14192d',
+                "&:hover": {
+                  backgroundColor: '#1e2a5a',
+                }
+              }}
+            >
+              Отправить
+            </Button>
+          </Box>
         </Stack>
-    )
+      );
 }
 
 export default Details;
