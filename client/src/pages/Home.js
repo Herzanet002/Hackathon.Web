6bfbd7a4a6640224ac12d0bb6a041d44e6a1fac6
ilydan5 <denis25072002@mail.ru>
2023-11-14 11:24:27 +0500

    Program added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,14 @@ import GetInTouch from '../components/GetInTouch';
 import GetStarted from '../components/GetStarted';
 import Header from '../components/Header';
 import Details from '../components/Details';
+import ForumProgram from '../components/ForumProgram';
 
 const Home = () => {
   return (
     <>
     <Header />
     <GetStarted />
+    <ForumProgram />
     <Details />
     </>
 
